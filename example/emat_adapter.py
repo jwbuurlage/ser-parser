f249c3aa052f99b1be07ee72f4fcec9d7a510c34
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2019-03-07 17:07:20 +0100

    Add watchdog to adapter

--- example/emat_adapter.py
+++ example/emat_adapter.py
@@ -1,6 +1,7 @@
 import math
 import ser_parser
 import os
+import time
 import numpy as np
 import tomop
 import argparse
@@ -14,14 +15,68 @@ parser.add_argument(
     '--host', default="localhost", help='the projection server host')
 parser.add_argument(
     '--port', type=int, default=5558, help='the projection server port')
+parser.add_argument(
+    '--prerecorded',default=False, help='use prerecorded set')
+parser.add_argument(
+    '--angles', type=int, nargs='+', help='list of angles')
+parser.add_argument(
+    '-a', type=int, help='angle start')
+parser.add_argument(
+    '-b', type=int, help='angle end')
+parser.add_argument(
+    '-i', type=int, help='increment')
+parser.add_argument(
+    '-m', type=int, default=1024, help='number of rows')
+parser.add_argument(
+    '-n', type=int, default=1024, help='number of columns')
 args = parser.parse_args()
+
+path = args.path;
+count = 0
+files = []
+angles = np.zeros((0))
+m, n = 0, 0
+
+if not args.prerecorded:
+    m = args.m
+    n = args.n
+    # angles = args.angles
+    angles = np.arange(args.a, args.b + 1, args.i)
+    count = len(angles)
+    files = list(map(lambda ang: (ang, path + "/" + str(ang) + "_1.ser"), angles))
+else:
+    def to_float(str):
+        return float(str[:-2])
+
+    for filename in os.listdir(path):
+        if filename.endswith(".ser"):
+            count += 1
+
+    files = [(0, "")] * count
+    angles = np.zeros(count)
+
+    i = 0
+    for filename in os.listdir(path):
+        if filename.endswith(".ser"):
+            full_path = os.path.join(path, filename)
+            angles[i] = to_float(os.path.splitext(filename)[0])
+            files[i] = (angles[i], full_path)
+            i += 1
+
+    (m, n), first_proj = ser_parser.parser(files[0][1])
+
+#files = sorted(files)
+#angles = np.sort(angles)
+print(files)
+
+angles = angles * (math.pi / 180.0)
+print(angles)
 
 
 def align(xs, ys):
     zs = ss.fftconvolve(xs, ys[::-1, ::-1])
     return np.array(np.unravel_index(np.argmax(zs, axis=None),
                                      zs.shape)) - np.array(xs.shape) + [1, 1]
-
 
 # start MATLAB
 #future = matlab.engine.start_matlab(async=True)
@@ -30,37 +85,6 @@ def align(xs, ys):
 
 pub = tomop.publisher(args.host, args.port)
 path = args.path
-
-count = 0
-for filename in os.listdir(path):
-    if filename.endswith(".ser"):
-        count += 1
-
-files = [(0, "")] * count
-angles = np.zeros(count)
-
-
-def to_float(str):
-    return float(str[:-2])
-
-
-i = 0
-for filename in os.listdir(path):
-    if filename.endswith(".ser"):
-        full_path = os.path.join(path, filename)
-        angles[i] = to_float(os.path.splitext(filename)[0])
-        files[i] = (angles[i], full_path)
-        i += 1
-
-(m, n), first_proj = ser_parser.parser(files[0][1])
-
-files = sorted(files)
-angles = np.sort(angles)
-print(files)
-
-angles = angles * (math.pi / 180.0)
-
-print(angles)
 
 # PACKET 1: object volume specification
 print("Sending object volume")
@@ -77,20 +101,44 @@ print("Sending scan data")
 pub.send(tomop.scan_settings_packet(0, 0, 0, True))
 
 # PACKET 4..: Projections
+while not os.path.isfile(files[0][1]):
+    time.sleep(0.01)
+
+time.sleep(1.0)
+
+shape, first_proj = ser_parser.parser(files[0][1])
+
 prev = np.reshape(first_proj, [m, n])
 prev_shift = np.round(ndimage.measurements.center_of_mass(prev)).astype(
     np.int) - np.array([m // 2 - 1, n // 2 - 1])
 
-for idx, (angle, filename) in enumerate(files):
-    print("Sending projection: ", idx)
-    shape, data = ser_parser.parser(filename)
+while len(files) > 0:
+    file_found = ""
+    angle_found = 0
+    idx_found = -1
+    while True:
+        for idx, (angle, filename) in enumerate(files):
+            if os.path.isfile(filename):
+                file_found = filename
+                angle_found = angle
+                idx_found = idx
+                break
+            time.sleep(0.01)
+        if (idx_found >= 0):
+            break
+        time.sleep(1.0)
+    print((angle_found, file_found))
+    files.remove((angle_found, file_found))
+
+    print("Sending projection: ", idx_found)
+    shape, data = ser_parser.parser(file_found)
     xs = np.reshape(data, shape)
     shift = align(xs, prev)
     print(shift)
     shifted_xs = np.roll(xs, -(prev_shift + shift), (0, 1))
 
     pub.send(
-        tomop.projection_packet(2, idx, [m, n],
+        tomop.projection_packet(2, idx_found, [m, n],
                                 np.ascontiguousarray(shifted_xs.ravel())))
 
     prev = xs
